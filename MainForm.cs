0b3489e1780edf7eaa084ba63afe32013dd7565f
Zhafran212 <120356728+Zhafran212@users.noreply.github.com>
2022-12-26 23:43:29 +0700

    Penambahan Menu Utama

---
+++ MainForm.cs
@@ -0,0 +1,45 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: FVCK-32
+ * Date: 18/01/2011
+ * Time: 18:27
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Data;
+using MySql.Data.MySqlClient;
+
+namespace penginapan
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		
+		void Button1Click(object sender, EventArgs e)
+		{
+			
+		}
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+	}
+}

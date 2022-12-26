e57584cf4c4ddc34396632e4cf321add05d7b21b
Mahendrasyah <105273780+Mahendrasyah@users.noreply.github.com>
2022-12-27 00:01:48 +0700

    revisi desain form tambah tamu by mahendrasyah

---
+++ FrmTambahTamu.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Data;
+
+namespace penginapan
+{
+	/// <summary>
+	/// Description of FrmTambahTamu.
+	/// </summary>
+	public partial class FrmTambahTamu : Form
+	{
+		public FrmTambahTamu()
+		{
+			InitializeComponent();
+		}
+		
+        private void FrmTambahTamu_Load(object sender, EventArgs e)
+        {
+
+        }
+	}
+}
